7e39a57c1c6a4b850b6613557b88a8d29d8f7cf1
amoljadhav531 <amoljadhav531@gmail.com>
2019-07-27 13:59:34 +0530

    Mapping with Entity TrainingRequest

--- src/main/java/com/hcl/trainingcenter/entity/TrainingRequest.java
+++ src/main/java/com/hcl/trainingcenter/entity/TrainingRequest.java
@@ -1,5 +1,36 @@
 package com.hcl.trainingcenter.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+
+@Entity
+@Data
 public class TrainingRequest {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long registrationId;
+	
+	private String course;
+	
+	private String proficiency;
+	
+	private String courseDate;
+	
+	private String status;
+	
+	@ManyToOne
+	@JoinColumn(name = "trainee_id")
+	private User traineeId;
+	
+	@ManyToOne
+	@JoinColumn(name = "trainer_id")
+	private User trainerId;
+	
 }
