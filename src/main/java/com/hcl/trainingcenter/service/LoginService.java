2fc4e24832d6ad27cbecb8361cbdd76a9319bb21
karanrathod123 <karanrathod2256@gmail.com>
2019-07-27 15:37:45 +0530

    @service removed

--- src/main/java/com/hcl/trainingcenter/service/LoginService.java
+++ src/main/java/com/hcl/trainingcenter/service/LoginService.java
@@ -1,11 +1,8 @@
 package com.hcl.trainingcenter.service;
 
-import org.springframework.stereotype.Service;
-
 import com.hcl.trainingcenter.dto.ResponseData;
 import com.hcl.trainingcenter.dto.UserDto;
 
-@Service
 public interface LoginService {
 
 	ResponseData loginUser(UserDto userDto);
