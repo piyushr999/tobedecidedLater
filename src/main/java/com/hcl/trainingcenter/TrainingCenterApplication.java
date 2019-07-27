b301da1138e198110253d6d70a7b42bb62d47c14
piyush <piyushr999@gmail.com>
2019-07-27 12:58:48 +0530

    Initial Setup

--- src/main/java/com/hcl/trainingcenter/TrainingCenterApplication.java
+++ src/main/java/com/hcl/trainingcenter/TrainingCenterApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.hcl.trainingcenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class TrainingCenterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(TrainingCenterApplication.class, args);
 	}
 
 }
