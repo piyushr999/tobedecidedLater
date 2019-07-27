ea0330873aa0df58198447b131272078f1532dd0
karanrathod123 <karanrathod2256@gmail.com>
2019-07-27 13:54:06 +0530

    dto package added

--- src/main/java/com/hcl/trainingcenter/controller/LoginController.java
+++ src/main/java/com/hcl/trainingcenter/controller/LoginController.java
@@ -1,5 +1,18 @@
 package com.hcl.trainingcenter.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
 public class LoginController {
 
+	public ResponseEntity<Object> loginUser() {
+
+		
+		return new ResponseEntity<>("", HttpStatus.OK);
+		 
+	}
 }
