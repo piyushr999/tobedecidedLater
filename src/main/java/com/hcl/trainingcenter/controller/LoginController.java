89f077ed6b6fd820a243fffc01d28ac2f1d82178
karanrathod123 <karanrathod2256@gmail.com>
2019-07-27 14:05:17 +0530

    loginService added

--- src/main/java/com/hcl/trainingcenter/controller/LoginController.java
+++ src/main/java/com/hcl/trainingcenter/controller/LoginController.java
@@ -1,17 +1,25 @@
 package com.hcl.trainingcenter.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hcl.trainingcenter.dto.UserDto;
+import com.hcl.trainingcenter.service.LoginService;
 
 @RestController
 @RequestMapping("/login")
 public class LoginController {
 
-	public ResponseEntity<Object> loginUser() {
+	@Autowired
+	private LoginService loginService;
 
-		return new ResponseEntity<>("", HttpStatus.OK);
+	public ResponseEntity<Object> loginUser(@RequestBody UserDto userDto) {
+
+		return new ResponseEntity<>(loginService.loginUser(userDto), HttpStatus.OK);
 
 	}
 }
--- src/main/java/com/hcl/trainingcenter/service/LoginService.java
+++ src/main/java/com/hcl/trainingcenter/service/LoginService.java
@@ -1,6 +1,10 @@
 package com.hcl.trainingcenter.service;
 
+import com.hcl.trainingcenter.dto.ResponseData;
+import com.hcl.trainingcenter.dto.UserDto;
 
 public interface LoginService {
+
+	ResponseData loginUser(UserDto userDto);
 
 }
